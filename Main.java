8f960fea4969c0857bcd876525c68e39808f3b06
komal <bankarkomal38@gmail.com>
2023-08-24 23:05:51 +0530

    Add Two integer number

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int First = 20;
+        int Second = 30;
+        // Add two number
+        int Sum = First + Second;
+        System.out.println(First + "+" + Second + " = " + Sum );
+    }
+}
