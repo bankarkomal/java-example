a51b6439ed095dcef2b4b7073dab616fe2071277
komal <bankarkomal38@gmail.com>
2023-08-26 22:18:07 +0530

    Multiply two float point numbers

---
+++ Example-practise/src/MultiplyTwoNumber.java
@@ -0,0 +1,11 @@
+public class MultiplyTwoNumber {
+    public static void main(String[] args) {
+        Float first = 4.7F;
+
+        Float Second = 3.9F;
+
+        Float product = first* Second;
+        
+        System.out.println("The product is:" + product);
+    }
+}
