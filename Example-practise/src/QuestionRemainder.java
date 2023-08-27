a48a989f6201041ae825af30d1d7087fba89621c
komal <bankarkomal38@gmail.com>
2023-08-27 22:11:57 +0530

    java program to compute Question and  Remainder

---
+++ Example-practise/src/QuestionRemainder.java
@@ -0,0 +1,13 @@
+public class QuestionRemainder {
+    public static void main(String[] args) {
+        int dividend = 25, divisor = 4;
+
+        int question = dividend / divisor;
+
+        int remainder = dividend % divisor;
+
+        System.out.println("Question = " + question);
+
+        System.out.println("Remainder = " + remainder);
+    }
+}
