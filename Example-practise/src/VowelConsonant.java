4c3fd1dc78c75decf515ed085cf6762c138be956
komal <bankarkomal38@gmail.com>
2023-08-28 22:25:21 +0530

    check weather alphabet is vowel or consonant using if-else statement

---
+++ Example-practise/src/VowelConsonant.java
@@ -0,0 +1,11 @@
+public class VowelConsonant {
+    public static void main(String[] args) {
+        int ch = 'i';
+
+        if (ch == 'a'|| ch == 'e' || ch == 'i'|| ch=='o' || ch == 'u')
+
+            System.out.println(ch + "is vowel");
+        else
+            System.out.println(ch + "is consonant");
+    }
+}
